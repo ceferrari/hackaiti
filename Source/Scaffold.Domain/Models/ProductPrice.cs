b23cd5f4bce1c5cfa43306256845a830a7d9a3da
Leandro Gonçalves de Oliveira <olegon.main@gmail.com>
2020-03-04 11:19:06 -0300

    Adding Product entity

---
+++ Source/Scaffold.Domain/Models/ProductPrice.cs
@@ -0,0 +1,9 @@
+namespace Scaffold.Domain.Models
+{
+    public class ProductPrice
+    {
+        public long Amount { get; set; }
+        public long Scale { get; set; }
+        public string CurrencyMode { get; set; }
+    }
+}
